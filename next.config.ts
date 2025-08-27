ba57ab82fc0facbc548dac23bf61ff216778a721
Serhii Bratkov <sir3720@gmail.com>
2025-08-27 13:58:29 +0300

    fix 3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ac.goit.global',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
